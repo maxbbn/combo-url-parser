cf27d6271f9c91f939b18b6c0ecae66f6bcab637
文龙 <wenlong@taobao.com>
2014-05-27 16:04:13 +0800

    fix port support

--- test/request.test.js
+++ test/request.test.js
@@ -1,11 +1,23 @@
 var request = require('request');
 
-request('http://10.125.202.112/abc.json', {
+//request('http://10.125.202.112/abc.json', {
+//    headers: {
+//        host: 't6l-tb-xcake-0-0-2.webtest.taobao.com'
+//    }
+//}, function(err, req, body){
+//    if (err) {
+//        console.log(err);
+//    }
+//});
+
+
+request('http://127.0.0.1:3333/abc.json', {
     headers: {
-        host: 't6l-tb-xcake-0-0-2.webtest.taobao.com'
+        host: '127.0.0.1:3333'
     }
 }, function(err, req, body){
     if (err) {
         console.log(err);
     }
+    console.log(body);
 });
